7d73fa89d4a31cc76d4d27cee787bbe4db775edf
lida24 <lidalenkova204@gmail.com>
2022-03-20 20:45:45 +0500

    fixed some little bugs

---
+++ src/icons/index.js
@@ -0,0 +1,14 @@
+import { FactorinLogo } from './FactorinLogo';
+import { Polygon } from './Polygon';
+import { IntroImage } from './IntroImage';
+import { OnlineApplicationImg } from './OnlineApplicationImg';
+import { Watch } from './Watch';
+
+
+export {
+    FactorinLogo,
+    Polygon,
+    IntroImage,
+    OnlineApplicationImg,
+    Watch,
+};
--- src/pages/OnlineApplication/index.js
+++ src/pages/OnlineApplication/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 import styles from './index.module.scss';
 import data from 'src/data';
-import OnlineApplicationImg from '../../icons/OnlineApplicationImg';
-import Watch from '../../icons/Watch';
+import { OnlineApplicationImg, Watch } from '../../icons';
 import { Button, cn, GridColumn, GridRow, Label, Text } from '../../ui2';
 
 
@@ -13,7 +12,7 @@ export const OnlineApplication = () => (
             <div className={styles.OnlineApplication_onlineApplication}>
                 <div className={styles.Intro}>
                     <GridRow>
-                        <GridColumn size={6}>
+                        <GridColumn size={7}>
                             <Label className={styles.Intro_container} textSize='40' weight='bold' component='h1'>
                                 Заявка на факторинг <br /> за <span className={styles.Intro_container_underline}>1 минут</span>у в любой банк
                                 <Text className={styles.Intro_container_head} textSize='24' component='p'>
@@ -22,22 +21,22 @@ export const OnlineApplication = () => (
                                 <Text className={styles.Intro_container_paragraph} textSize='18' component='p'>
                                     Услуга будет доступна в ближайшее время.
                                 </Text>
-                                <Text className={styles.Intro_container_paragraph_red} textSize='18' component='p'>
+                                <Label className={styles.Intro_container_paragraph_red} textSize='18' weight='medium' component='p'>
                                     Получите ранний доступ, чтобы первыми обратиться к банкам.
-                                </Text>
+                                </Label>
                             </Label>
                             <Button className={styles.Intro_container_button} variant='normal' size='64' type='styled'>
                                 Получить ранний доступ
                             </Button>
                         </GridColumn>
-                        <GridColumn size={10}>
+                        <GridColumn size={5}>
                             <OnlineApplicationImg />
                         </GridColumn>
                     </GridRow>
                 </div>
                 <div className={styles.About}>
                     <GridRow>
-                        <GridColumn size={6}>
+                        <GridColumn size={7}>
                             <Label textSize='32' weight='bold' component='h1'>
                                 Что такое заявка на факторинг?
                             </Label>
@@ -50,7 +49,10 @@ export const OnlineApplication = () => (
                             </Text>
                             <div className={styles.About_head_section}>
                                 <Text className={styles.About_head_header} textSize='18' weight='bold' component='p'>
-                                    Единственная площадка, где зарегистрировано сразу <span className={styles.About_head_header_underline}>40 банков и факторинговых компаний</span>.
+                                    Единственная площадка, где зарегистрировано сразу
+                                    <span className={styles.About_head_header_underline}>40 банков&nbsp;</span>
+                                    <span className={styles.About_head_header_underline}>и&nbsp;факторинговых&nbsp;</span>
+                                    <span className={styles.About_head_header_underline}>компаний.</span>
                                 </Text>
                                 <Text className={styles.About_head_paragraph} textSize='18' component='p'>
                                     Они предоставят вам информацию о текущей стоимости финансирования кассовых разрывов при работе с отсрочкой платежа всего по одной онлайн-заявке.
@@ -58,30 +60,26 @@ export const OnlineApplication = () => (
                                 <Button variant='secondary' size='36' type='styled'>Узнать подробнее о факторинге</Button>
                             </div>
                             <div className={styles.About_section}>
-                                {data.aboutRows.map((content, index) => (
-                                    <Text key={index} textSize='18' component='p'>
-                                        {content.rows.map((column, i) => {
-                                            if (i % 2 === 0) {
-                                                return (
-                                                    <Text className={styles.About_section_head_rows} key={i} textSize='18' component='p'>
-                                                        {column}
-                                                    </Text>
-                                                );
-                                            }
-                                            return (
-                                                <Text className={styles.About_section_rows} key={i} textSize='18' component='p'>
-                                                    {column}
-                                                </Text>
-                                            );
-                                        })}
-                                    </Text>
-                                ))}
+                                {data.aboutRows.map((content, index) => {
+                                    if (index % 2 === 0) {
+                                        return (
+                                            <Text className={styles.About_section_head_rows} key={index} textSize='18' component='p'>
+                                                {content}
+                                            </Text>
+                                        );
+                                    }
+                                    return (
+                                        <Text className={styles.About_section_rows} key={index} textSize='18' component='p'>
+                                            {content}
+                                        </Text>
+                                    );
+                                })}
                             </div>
                             <Text className={styles.About_last_paragraph} textSize='18' component='p'>
                                 О всех последующих обновлениях будем рассказывать на сайте, в социальных сетях и рассылках компании.
                             </Text>
                         </GridColumn>
-                        <GridColumn size={12}>
+                        <GridColumn size={5}>
                             <Watch />
                         </GridColumn>
                     </GridRow>
